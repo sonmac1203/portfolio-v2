a059fd1594b1ec6f13ce7e9363bdeddfae0b167d
Son Mac <mactranthienson@gmail.com>
2023-07-25 09:16:42 -0700

    Add SectionTitle component

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,3 +1,4 @@
 export { CoreContainer } from './CoreContainer';
 export { LeftColumn } from './LeftColumn';
 export { RightColumn } from './RightColumn';
+export { SectionTitle } from './SectionTitle';
