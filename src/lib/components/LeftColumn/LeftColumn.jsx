aad95d33b4248b99af9612e7c46dc2524238aea6
Son Mac <mactranthienson@gmail.com>
2023-07-25 09:29:48 -0700

    Update LeftCol and RightCol

--- src/lib/components/LeftColumn/LeftColumn.jsx
+++ src/lib/components/LeftColumn/LeftColumn.jsx
@@ -1,9 +1,11 @@
 import styles from './LeftColumn.module.css';
 
-export const LeftColumn = () => {
+export const LeftColumn = ({ children }) => {
   return (
-    <div className={`${styles.Main} sm:h-screen sm:sticky sm:top-0`}>
-      LeftColumn
+    <div
+      className={`${styles.Main} sm:h-screen sm:sticky sm:top-0 flex flex-col pt-20 pb-10`}
+    >
+      {children}
     </div>
   );
 };
--- src/lib/components/RightColumn/RightColumn.jsx
+++ src/lib/components/RightColumn/RightColumn.jsx
@@ -1,5 +1,9 @@
 import styles from './RightColumn.module.css';
 
 export const RightColumn = ({ children }) => {
-  return <div className={styles.Main}>{children}</div>;
+  return (
+    <div className={`${styles.Main} flex flex-col gap-y-5 pb-10`}>
+      {children}
+    </div>
+  );
 };
