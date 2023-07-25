5d6abc6456c49324dff299b548e5b76c7c086120
Son Mac <mactranthienson@gmail.com>
2023-07-25 09:14:38 -0700

    Add fontawesome script

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,6 +12,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <head>
+        <Script
+          async
+          src='https://kit.fontawesome.com/ead8c78b5a.js'
+          crossOrigin='anonymous'
+          strategy='afterInteractive'
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
