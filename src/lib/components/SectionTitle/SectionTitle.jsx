d4986e227899faabc68f610cc03321f48e67c152
Son Mac <mactranthienson@gmail.com>
2023-07-26 16:33:20 -0700

    Update SectionTitle

--- src/lib/components/SectionTitle/SectionTitle.jsx
+++ src/lib/components/SectionTitle/SectionTitle.jsx
@@ -1,3 +1,12 @@
-export const SectionTitle = ({ children }) => {
-  return <h2 className='text-3xl'>{children}</h2>;
+export const SectionTitle = ({ children, className }) => {
+  return (
+    <div
+      className={`flex items-center gap-x-2 uppercase lg:hidden text-ivory ${
+        className || ''
+      }`}
+    >
+      <span className='h-px w-8 bg-ash' />
+      <h2>{children}</h2>
+    </div>
+  );
 };
