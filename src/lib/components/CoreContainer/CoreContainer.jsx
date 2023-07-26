18fc710c1168a03d9b89e365a248f137b88c5191
Son Mac <mactranthienson@gmail.com>
2023-07-26 16:34:43 -0700

    Update layout of CoreContainer

--- src/lib/components/CoreContainer/CoreContainer.jsx
+++ src/lib/components/CoreContainer/CoreContainer.jsx
@@ -1,6 +1,6 @@
 export const CoreContainer = ({ children }) => {
   return (
-    <div className='flex flex-col max-w-4xl mx-auto bg-slate-300 min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0 sm:flex-row'>
+    <div className='max-w-4xl mx-auto min-h-screen max-w-screen-xl px-10 pt-12 md:px-12 md:pt-20 lg:px-24 lg:py-0 md:grid lg:grid-cols-3 lg:gap-x-8'>
       {children}
     </div>
   );
