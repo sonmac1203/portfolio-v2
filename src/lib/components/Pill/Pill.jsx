c4c9a98774713b0ef72fa82cb3cedb5f7b00dfe8
Son Mac <mactranthienson@gmail.com>
2023-07-26 11:33:19 -0700

    Add Pill component

---
+++ src/lib/components/Pill/Pill.jsx
@@ -0,0 +1,7 @@
+export const Pill = ({ text }) => {
+  return (
+    <div className='bg-indigo inline-block rounded-md py-1 px-2 text-bright'>
+      {text}
+    </div>
+  );
+};
--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -2,3 +2,4 @@ export { CoreContainer } from './CoreContainer';
 export { LeftColumn } from './LeftColumn';
 export { RightColumn } from './RightColumn';
 export { SectionTitle } from './SectionTitle';
+export { Pill } from './Pill';
