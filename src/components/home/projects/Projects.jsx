165a56dedda8a16e747c571cec607941e2cc5cd0
Son Mac <mactranthienson@gmail.com>
2023-07-25 22:55:28 -0700

    Update Project

--- src/components/home/projects/Projects.jsx
+++ src/components/home/projects/Projects.jsx
@@ -1,17 +1,74 @@
 import { SectionTitle } from '@/lib/components';
 import { ProjectCard } from './components';
+import { useEffect, useContext } from 'react';
+import { SectionObserverContext } from '@/lib/context';
 
 export const Projects = ({ sectionRef }) => {
-  const projects = [];
+  const projects = [
+    {
+      name: 'Antaris',
+      description:
+        'Leading a team of 5 engineering students to create a cutting-edge health monitoring system that seamlessly connects researchers and patients through Amazon Alexa devices, complemented by a full-stack web application built in Next.js.',
+      photoName: '',
+      tech: '',
+    },
+    {
+      name: 'Pulses - The health tracker',
+      description:
+        'A 3-person project that provides a web-based personal dashboard to visualize heart rate and blood oxygen saturation level measured from an IoT-enabled management board of Particle.',
+      photoName: '',
+      tech: '',
+    },
+
+    {
+      name: 'SocialHub360',
+      description:
+        'A personal project featuring a React-Firebase web app that enables all-in-one content management on Facebook and Instagram for restaurant and shop owners.',
+      photoName: '',
+      tech: '',
+    },
+
+    {
+      name: 'Restaurant Admin Tool',
+      description:
+        'Simulated Java-powered restaurant administration tool for managers/chefs, and customers to perform different kinds of real-life tasks such as managing staff and ordering food.',
+      photoName: '',
+      tech: '',
+    },
+
+    {
+      name: 'Between Us',
+      description:
+        'A deployed website serving as the birthday present for my partner featuring creating a photo gallery, marking memorable events, exchanging notes, and keeping track of elapsed time.',
+      photoName: '',
+      tech: '',
+    },
+  ];
+  const { setCurrentSection } = useContext(SectionObserverContext);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const rect = sectionRef.current.getBoundingClientRect();
+      if (rect.bottom >= 0 && rect.top <= 20) {
+        setCurrentSection('projects');
+      }
+    };
+    window.removeEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
     <section id='projects' className='pt-20' ref={sectionRef}>
       {/* <SectionTitle>Projects</SectionTitle> */}
       <ul className='list-none flex flex-col gap-y-4'>
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {projects.map((p, k) => (
+          <ProjectCard
+            name={p.name}
+            description={p.description}
+            photoName={p.photoName}
+            tech={p.tech}
+          />
+        ))}
       </ul>
     </section>
   );
--- src/components/home/projects/components/ProjectCard/ProjectCard.jsx
+++ src/components/home/projects/components/ProjectCard/ProjectCard.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import Image from 'next/image';
 
-export const ProjectCard = () => {
+export const ProjectCard = ({ name, description, photoName, tech }) => {
   return (
     <li className='grid grid-cols-11 gap-x-3 items-center'>
       <div className='col-span-3'>
@@ -14,18 +13,12 @@ export const ProjectCard = () => {
       </div>
       <div className='col-span-8'>
         <div className='flex justify-between'>
-          <h3 className='text-xl'>This is a title</h3>
+          <h3 className='text-xl'>{name}</h3>
           <div className='flex'>
             <span>Team</span>
           </div>
         </div>
-        <p>
-          Made and deployed a React web app providing all-in-one social media
-          management for restaurant and shop owners. Integrated Facebook APIs to
-          enable publishing, updating, deleting, viewing, and scheduling content
-          on business accounts. Leveraged Firebase for email-password
-          authentication and data storage.
-        </p>
+        <p>{description}</p>
         <div>Tech</div>
       </div>
     </li>
