511e9c6ba8f7ae26c7f620dc36a7e5b74704683e
Son Mac <mactranthienson@gmail.com>
2023-07-26 16:59:44 -0700

    Update links for footers

--- src/components/home/contactfooter/ContactFooter.jsx
+++ src/components/home/contactfooter/ContactFooter.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-
 export const ContactFooter = () => {
   return (
-    <ul className='flex text-xl mt-5 lg:w-full lg:flex-row gap-x-6 lg:text-2xl lg:mt-auto text-bright'>
-      <i className='fa-brands fa-linkedin' />
-      <i className='fa-brands fa-github' />
-      <i className='fa-brands fa-instagram' />
-      <i className='fa-solid fa-envelope' />
+    <ul className='flex text-xl mt-5 gap-x-4 lg:w-full lg:flex-row lg:gap-x-6 lg:text-2xl lg:mt-auto text-bright'>
+      <a href='https://www.linkedin.com/in/sonmac1203' target='_blank'>
+        <i className='fa-brands fa-linkedin' />
+      </a>
+      <a href='https://github.com/sonmac1203' target='_blank'>
+        <i className='fa-brands fa-github' />
+      </a>
+      <a href='https://www.instagram.com/sont.mac' target='_blank'>
+        <i className='fa-brands fa-instagram' />
+      </a>
+      <a href='mailto:sontmac1203@gmail.com'>
+        <i className='fa-solid fa-envelope' />
+      </a>
     </ul>
   );
 };
--- src/components/home/techfooter/TechFooter.jsx
+++ src/components/home/techfooter/TechFooter.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
-
 export const TechFooter = () => {
+  const nextLink = 'https://nextjs.org';
+  const tailwindLink = 'https://tailwindcss.com';
+  const brittanyLink = 'https://brittanychiang.com';
+  const sourceCodeLink = 'https://github.com/sonmac1203/portfolio-v2';
+
   return (
     <footer className='mt-auto text-center text-xs md:text-sm pt-20'>
-      Developed in <span className='text-bright'>Next.js 13</span> with{' '}
-      <span className='text-bright'>Tailwind CSS</span> by Son Mac in 2023.
-      Inspired by Brittany Chang. Source code can be found here. You can also
-      see the previous version here.
+      Developed in{' '}
+      <a className='text-bright' href={nextLink} target='_blank'>
+        Next.js 13
+      </a>{' '}
+      with{' '}
+      <a className='text-bright' href={tailwindLink} target='_blank'>
+        Tailwind CSS
+      </a>{' '}
+      by Son Mac in 2023. Greatly inspired by{' '}
+      <a className='text-bright' href={brittanyLink} target='_blank'>
+        Brittany Chang
+      </a>
+      . Source code can be found{' '}
+      <a className='text-bright' href={sourceCodeLink} target='_blank'>
+        here
+      </a>
+      . You can also see the previous version here.
     </footer>
   );
 };
