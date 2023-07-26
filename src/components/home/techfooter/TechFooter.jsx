d970727383507877ca9d470324cbba9ba6ca39ab
Son Mac <mactranthienson@gmail.com>
2023-07-26 16:35:08 -0700

    Update size responsiveness for TechFooter

--- src/components/home/techfooter/TechFooter.jsx
+++ src/components/home/techfooter/TechFooter.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 export const TechFooter = () => {
   return (
-    <footer className='mt-auto text-center text-sm'>
-      Developed in Next.js 13 with Tailwind CSS by Son Mac in 2023. Inspired by
-      Brittany Chang. Source code can be found here. You can also see the
-      previous version here.
+    <footer className='mt-auto text-center text-xs md:text-sm pt-20'>
+      Developed in <span className='text-bright'>Next.js 13</span> with{' '}
+      <span className='text-bright'>Tailwind CSS</span> by Son Mac in 2023.
+      Inspired by Brittany Chang. Source code can be found here. You can also
+      see the previous version here.
     </footer>
   );
 };
