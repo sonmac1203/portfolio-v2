d5c323a34ce130a7eac6e49f95cbe1570be8e900
Son Mac <mactranthienson@gmail.com>
2024-01-28 22:35:16 -0800

    Update text in Experience & fix lint warnings

--- src/components/home/experience/Experience.jsx
+++ src/components/home/experience/Experience.jsx
@@ -15,18 +15,28 @@ export const Experience = ({ sectionRef }) => {
     window.removeEventListener('scroll', handleScroll);
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+  }, [sectionRef, setCurrentSection]);
 
   const content = [
     {
       title: 'Software Engineer',
+      employer: 'Vanta, Inc.',
+      description:
+        'Build and deliver to secure the Internet and protect consumer data.',
+      time: 'Jan 2024 - Present',
+      location: 'San Francisco, CA',
+      href: 'https://www.vanta.com',
+      tech: 'React,TypeScript,GraphQL',
+    },
+    {
+      title: 'Software Developer',
       employer: 'kmap.arizona.edu',
       description:
-        "Consistently deliver quality production code, improving KMap's web interface, and contribute to key projects, including the migration to Next.js 12 and onboarding page redesign. Implement and streamline internal UI components for development efficiency and maintain effective communication with data team over the management of public and private API endpoints.",
-      time: 'Feb 2022 - Present',
+        "Consistently deliver quality production code, improving KMap's web interface, and contribute to key projects, including the migration to Next.js (12 & 13) and onboarding page redesign. Implement and streamline internal UI components for development efficiency and maintain effective communication with data team over the management of public and private API endpoints.",
+      time: 'Feb 2022 - Jan 2024',
       location: 'Tucson, AZ',
       href: 'https://kmap.arizona.edu',
-      tech: 'Next,JavaScript,MongoDB,Python,D3,Mapbox',
+      tech: 'Next,TS/JS,MongoDB,Python,D3,Mapbox',
     },
     {
       title: 'Frontend Developer Intern',
